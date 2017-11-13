92b64d7e1b58703fa1b52c790dda3e31098530d9
Prieya <pnaraine@aoiths.org>
2017-11-13 22:11:58 +0000

    hello

--- db-search-functions.js
+++ db-search-functions.js
@@ -10,21 +10,54 @@ function getVersion(handleVersion) {
     // the database snapshot and passes it as an argument to the 'handleVersion'
     // callback.
     
-    //var version = snapshot.val();
-    //handleVersion(version);
+    var version = snapshot.val();
+    handleVersion(version);
   });
 }
 
 function getHelloWorldValue(handleType) {
-  // TODO: Your code goes here. Hint: use 'getVersion' as a template.
+        var database = firebase.database();
+  var databaseVersionReference = database.ref('/db-search/hello-world');
+  databaseVersionReference.once('value').then(function(snapshot) {
+    
+    // You found me! Great job!
+    // TODO: Uncomment the code below. It gets the current version value from
+    // the database snapshot and passes it as an argument to the 'handleVersion'
+    // callback.
+    
+    var version = snapshot.val();
+    handleType(version);
+  });
 }
 
 function applyDiscount(handleNewPrice) {
-  // TODO: Your code goes here.
+         var database = firebase.database();
+  var databaseVersionReference = database.ref('/db-search/discount');
+  databaseVersionReference.once('value').then(function(snapshot) {
+    
+    // You found me! Great job!
+    // TODO: Uncomment the code below. It gets the current version value from
+    // the database snapshot and passes it as an argument to the 'handleVersion'
+    // callback.
+    
+    var version = snapshot.val();
+    handleNewPrice(version*100);
+  }); // TODO: Your code goes here.
 }
 
 function getArrayLength(handleLength) {
-  // TODO: Your code goes here.
+          var database = firebase.database();
+  var databaseVersionReference = database.ref('/db-search/array');
+  databaseVersionReference.once('value').then(function(snapshot) {
+    
+    // You found me! Great job!
+    // TODO: Uncomment the code below. It gets the current version value from
+    // the database snapshot and passes it as an argument to the 'handleVersion'
+    // callback.
+    
+    var version = snapshot.val();
+    handleLength(version);
+  });
 }
 
 function getLastArrayElement(handleElement) {
